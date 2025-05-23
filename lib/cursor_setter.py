675fab523f7b186d85966a6ab349598ab77d20f5
hite404 <hite404@qq.com>
2025-05-23 21:04:31 +0800

    fix: 修复鼠标指针过大时，设置系统指针时显示大小不正确的问题

--- lib/cursor_setter.py
+++ lib/cursor_setter.py
@@ -8,7 +8,7 @@ from winreg import HKEYType
 from win32con import IMAGE_CURSOR, LR_LOADFROMFILE, OCR_NORMAL, OCR_APPSTARTING, OCR_WAIT, OCR_CROSS, OCR_IBEAM, OCR_NO, \
     OCR_SIZENS, OCR_SIZEWE, OCR_SIZENWSE, OCR_SIZENESW, OCR_UP, OCR_HAND, OCR_SIZEALL, IDC_ARROW, IDC_HELP, \
     IDC_APPSTARTING, IDC_WAIT, IDC_CROSS, IDC_IBEAM, IDC_SIZENS, IDC_NO, IDC_HAND, IDC_UPARROW, IDC_SIZEWE, \
-    IDC_SIZENESW, IDC_SIZEALL
+    IDC_SIZENESW, IDC_SIZEALL, LR_DEFAULTSIZE
 from win32gui import LoadImage, LoadCursor, CopyIcon
 
 from lib.log import logger
@@ -211,10 +211,13 @@ def set_cursors_progress(cursors_info: CursorsInfo, scheme_type: SchemesType, sc
                 CopyIcon(cursor)
                 path = ""
             else:
-                cursor = LoadImage(None, cursor_data.cursor_path, IMAGE_CURSOR, 0, 0, LR_LOADFROMFILE)
+                cursor = LoadImage(None, cursor_data.cursor_path, IMAGE_CURSOR, 0, 0, LR_LOADFROMFILE | LR_DEFAULTSIZE)
                 path = cursor_data.cursor_path
             winreg.SetValueEx(global_set, cursor_data.kind.value, 0, winreg.REG_EXPAND_SZ, path)
-            SetSystemCursor(cursor, cursor_data.ocr_con)
+            print(cursor)
+            ret = SetSystemCursor(cursor, cursor_data.ocr_con)
+            if ret == 0:
+                logger.error(f"设置 {cursor_data.kind.value} 失败")
     except OSError:
         yield False, -1
     yield True, -1

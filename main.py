883118a2eaa8a49fafb6b72497a7faeddc36d827
hite404 <hite404@qq.com>
2025-08-30 22:01:52 +0800

    feat: 打包方案适配

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import faulthandler
 faulthandler.enable()
 
 import ctypes
+import sys
+import os
 
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("MineCursor")
+
+os.chdir(os.path.dirname(__file__))  # 进入当前目录
+sys.path.append(os.path.dirname(__file__))  # 添加模块导入路径
 
 import wx
 from ui_ctl.theme_editor import ThemeEditor

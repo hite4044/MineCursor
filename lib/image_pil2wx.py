c5a86ac55a32e80e2bfeacbc9af6458ec732a46e
hite404 <hite404@qq.com>
2025-10-07 22:24:16 +0800

    feat: 优化性能?

--- lib/image_pil2wx.py
+++ lib/image_pil2wx.py
@@ -6,7 +6,7 @@ def PilImg2WxImg(image: Image.Image) -> wx.Image:
     """PIL的Image转化为wxImage"""
     bitmap: wx.Image = wx.Image(image.size[0], image.size[1])
     bitmap.SetData(image.convert("RGB").tobytes())
-    bitmap.alpha_buffer = image.convert("RGBA").tobytes()[3::4]
+    bitmap.alpha_buffer = image.convert("RGBA").getchannel("A").tobytes()
     bitmap.SetAlphaBuffer(bitmap.alpha_buffer)
     return bitmap
 

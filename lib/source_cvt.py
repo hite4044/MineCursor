487e4b895203bca3da1b7afc56cd7253d4324769
hite404 <hite404@qq.com>
2026-02-14 18:16:03 +0800

    feat: 支持载入NeoForge、Fabric模组作为素材源

--- lib/source_cvt.py
+++ lib/source_cvt.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 from io import BytesIO
 from os.path import join, split
 from shutil import copy as copy_file
+from typing import Any
 from zipfile import ZipFile, ZIP_DEFLATED
 
 import toml
@@ -12,7 +13,6 @@ from PIL import Image
 
 from lib.data import AssetSource
 from lib.datas.base_struct import generate_id
-
 
 
 def check_names(file: ZipFile, names: list[str]) -> bytes | None:
@@ -23,6 +23,7 @@ def check_names(file: ZipFile, names: list[str]) -> bytes | None:
 
 
 def append_basic_info(file: ZipFile, note: str) -> str:
+    """添加README、MCMETA、License文件的内容进入描述"""
     if info := check_names(file, ["pack.mcmeta"]):
         note += "\n资源包信息 (Mcmeta): \n"
         note += info.decode("utf-8", errors="ignore")
@@ -38,41 +39,97 @@ def append_basic_info(file: ZipFile, note: str) -> str:
     return note
 
 
-def load_jar2source(fp: str, extract_dir: str = None):
+class ModInfo:
+    def __init__(self):
+        self.name: str = ""
+        self.mod_id: str = ""
+        self.version: str = ""
+        self.authors: str = ""
+        self.description: str = ""
+        self.icon: str | None = None
+        self.license: str | None = None
+        self.uri: str | None = None
+
+        self.source_id: str = ""
+
+    def load_as_forge(self, info: dict[str, Any], jar_version: str = None):
+        mod = info["mods"][0]
+        self.name = mod.get("displayName", "新素材源")
+        self.mod_id = mod.get("modId", "what, this mod has no modId")
+        self.version = mod.get("version", "未知")
+        if "${file.jarVersion}" in self.version and jar_version:
+            self.version = self.version.replace("${file.jarVersion}", jar_version)
+        self.authors = mod.get("authors", "未知")
+        self.description = mod.get("description", "未知")
+        self.icon = mod.get("logoFile")
+        self.source_id = f"{self.name}-{self.version}-{hex(hash(str(info)))[3:3 + 8]}"
+        self.license = info.get('license')
+        self.uri = mod.get('displayURL')
+
+    def load_as_fabric(self, info: dict[str, Any]):
+        self.name = info.get("name", "新素材源")
+        self.mod_id = info.get("id", "fabric-mod")
+        self.version = info.get("version", "未知")
+        self.authors = ", ".join(info.get("authors", ["未知"]))
+        self.description = info.get("description", "未知")
+        self.icon = info.get("icon")
+        self.source_id = f"{self.name}-{self.version}-{hex(hash(str(info)))[3:3 + 8]}"
+        if license_list := info.get('license'):
+            self.license = ", ".join(license_list)
+        self.uri = info.get('contact', {}).get('homepage')
+
+
+def load_jar2source(fp: str, extract_dir: str = None) -> AssetSource:
     jar = ZipFile(fp)
-    info_bytes = jar.read("META-INF/mods.toml")
-    info = toml.loads(info_bytes.decode("utf-8"))
+    if "META-INF/mods.toml" in jar.NameToInfo:  # Forge
+        info_bytes = jar.read("META-INF/mods.toml")
+        forge_or_fabric = True
+    elif "META-INF/neoforge.mods.toml" in jar.NameToInfo:  # NeoForge
+        info_bytes = jar.read("META-INF/neoforge.mods.toml")
+        forge_or_fabric = True
+    elif "fabric.mod.json" in jar.NameToInfo:  # Fabric
+        info_bytes = jar.read("fabric.mod.json")
+        forge_or_fabric = False
+    else:
+        raise NotImplementedError("不支持的模组格式")
 
     # 提取模组信息
-    mods = info["mods"][0]
-    mod_id = mods["modId"]
-    name = mods.get("displayName", "新素材源")
-    version = mods.get("version", "未知")
-    authors = mods.get("authors", "未知")
-    description = mods.get("description", "未知")
-    source_id = f"{name}-{version}-{hex(hash(info_bytes))[3:3 + 8]}"
-    note = ""
-    if info.get("license"):
-        note += f"模组协议: {info.get('license')}\n"
-    if mods.get("displayURL"):
-        note += f"模组主页: {mods['displayURL']}\n"
+    info = ModInfo()
+    if forge_or_fabric:  # Forge & Neoforge
+        jar_version = None
+        if "META-INF/MANIFEST.MF" in jar.NameToInfo:  # 如果有jar信息文件, 就提取
+            mf = jar.read("META-INF/MANIFEST.MF").decode("utf-8")
+            result_list = re.findall(r"Implementation-Version: .+\n", mf)
+            if result_list:
+                data_line: str = result_list[0]
+                jar_version = data_line.rstrip("\n").rstrip("\r").replace("Implementation-Version: ", "")
+        info_dict = toml.loads(info_bytes.decode("utf-8"))
+        info.load_as_forge(info_dict, jar_version)
+    else:  # Fabric!
+        info_dict = json.loads(info_bytes.decode("utf-8"))
+        info.load_as_fabric(info_dict)
+    note = ""  # 添加额外信息进入描述
+    if info.license:
+        note += f"模组协议: {info.license}\n"
+    if info.uri:
+        note += f"模组主页: {info.uri}\n"
     note = append_basic_info(jar, note)
 
     # 保存图标
-    if icon := mods.get("logoFile"):
+    if icon := info.icon:
         image = Image.open(BytesIO(jar.read(icon)))
         image = image.convert("RGBA")
         image.save(join(extract_dir, "icon.png"), "PNG")
 
     # 提取贴图
     texture_zip = ZipFile(join(extract_dir, "textures.zip"), "x", ZIP_DEFLATED, compresslevel=1)
-    textures_root = f"assets/{mod_id}/textures/"
-    for path, info in jar.NameToInfo.items():
+    textures_root = f"assets/{info.mod_id}/textures/"
+    for path, zip_info in jar.NameToInfo.items():
         if path.startswith(textures_root) and path != textures_root:
-            new_info = deepcopy(info)
-            new_info.filename = info.filename.replace(textures_root, "", 1)
-            new_info.orig_filename = info.orig_filename.replace(textures_root, "", 1)
-            if info.is_dir():
+            new_info = deepcopy(zip_info)
+            new_info.filename = zip_info.filename.replace(textures_root, "", 1)
+            new_info.orig_filename = zip_info.orig_filename.replace(textures_root, "", 1)
+            if zip_info.is_dir():
                 texture_zip.filelist.append(new_info)
             else:
                 texture_zip.writestr(new_info, jar.read(path))
@@ -84,7 +141,7 @@ def load_jar2source(fp: str, extract_dir: str = None):
     with open(join(extract_dir, "orig_filename"), "w", encoding="utf-8") as f:
         f.write(split(fp)[1])
 
-    source = AssetSource(name, source_id, version, authors, description, note, extract_dir)
+    source = AssetSource(info.name, info.source_id, info.version, info.authors, info.description, note, extract_dir)
     return source
 
 
